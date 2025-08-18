1e884c36191d2613279981bc0381e94efeaac35e
Andrei Iakovlev <a.yakovlev@virtusystems.ru>
2025-08-18 10:47:52 +0300

    Увеличил время таймаута при ожидании ответа от сервера

--- client-front-module/src/api/config.ts
+++ client-front-module/src/api/config.ts
@@ -6,7 +6,7 @@ console.log("[API] baseURL =", baseURL);
 
 export const client = axios.create({
     baseURL,
-    timeout: 5000,
+    timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
     },
